3a0dbe4f1322eeb963263340a533dbef75fc30d6
Gourav Banerjee <39580718+C713Gb@users.noreply.github.com>
2020-10-24 19:40:45 +0530

    Updating Pickup Status to Database done

--- app/src/main/java/com/example/mybus/activities/PickupActivity.java
+++ app/src/main/java/com/example/mybus/activities/PickupActivity.java
@@ -6,7 +6,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,6 +17,8 @@ import android.widget.Toast;
 import com.example.mybus.R;
 import com.example.mybus.adapters.PickupAdapter;
 import com.example.mybus.models.Pickup;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,6 +26,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PickupActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
@@ -32,6 +37,8 @@ public class PickupActivity extends AppCompatActivity implements SwipeRefreshLay
     ArrayList<Pickup> pickupArrayList;
     PickupAdapter pickupAdapter;
     Button update;
+    public HashMap<String,String> pickupMap;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,11 +61,60 @@ public class PickupActivity extends AppCompatActivity implements SwipeRefreshLay
             }
         });
 
+        pd = new ProgressDialog(PickupActivity.this);
+
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Update status
-                Toast.makeText(PickupActivity.this, pickupAdapter.checked, Toast.LENGTH_SHORT).show();
+                pd.setMessage("Updating...");
+                pd.show();
+
+                reference = FirebaseDatabase.getInstance().getReference("Pickups");
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        try {
+                            final int[] k = {-1};
+
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                Pickup pickup = snapshot.getValue(Pickup.class);
+                                String place = pickup.getPlaceName();
+                                String id = pickup.getId();
+                                String updatedStatus = pickupMap.get(place);
+
+                                reference.child(id).child("status").setValue(updatedStatus).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        pd.dismiss();
+                                        if (task.isSuccessful()){
+                                            // Updated
+                                            k[0] = 1;
+                                        } else {
+                                            // Update Failed
+                                            Toast.makeText(PickupActivity.this, "Update Failed", Toast.LENGTH_SHORT).show();
+                                            Log.d("UPDATE", task.getException().getMessage());
+                                            k[0] = 0;
+                                        }
+                                    }
+                                });
+
+                                if (k[0] == 0) {
+                                    break;
+                                }
+                            }
+                        } catch (Exception e){
+                            pd.dismiss();
+                            Toast.makeText(PickupActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
             }
         });
 
@@ -70,6 +126,7 @@ public class PickupActivity extends AppCompatActivity implements SwipeRefreshLay
 
         try {
 
+            pickupMap = new HashMap<>();
             reference = FirebaseDatabase.getInstance().getReference("Pickups");
 
             pickupArrayList = new ArrayList<>();
@@ -80,6 +137,9 @@ public class PickupActivity extends AppCompatActivity implements SwipeRefreshLay
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
                     for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                         Pickup pickup = dataSnapshot.getValue(Pickup.class);
+                        String place = pickup.getPlaceName();
+                        String status = pickup.getStatus();
+                        pickupMap.put(place,status);
                         pickupArrayList.add(pickup);
                     }
 
--- app/src/main/java/com/example/mybus/adapters/PickupAdapter.java
+++ app/src/main/java/com/example/mybus/adapters/PickupAdapter.java
@@ -15,6 +15,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.mybus.R;
 import com.example.mybus.activities.PickupActivity;
 import com.example.mybus.models.Pickup;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -41,11 +43,22 @@ public class PickupAdapter extends RecyclerView.Adapter<PickupAdapter.MyViewHold
     @Override
     public void onBindViewHolder(@NonNull PickupAdapter.MyViewHolder holder, int position) {
         holder.text.setText(pickupArrayList.get(position).getPlaceName());
-        String status = pickupArrayList.get(position).getStatus();
-        if (status.equals("true")) {
-            holder.aSwitch.setChecked(true);
-        } else {
-            holder.aSwitch.setChecked(false);
+        try {
+            String status = pickupArrayList.get(position).getStatus();
+            if (status.equals("true")) {
+                holder.aSwitch.setChecked(true);
+            } else {
+                holder.aSwitch.setChecked(false);
+            }
+        } catch (Exception e) {
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Pickups");
+            ref.child(pickupArrayList.get(position).getId()).child("status").setValue("true");
+            String status = pickupArrayList.get(position).getStatus();
+            if (status.equals("true")) {
+                holder.aSwitch.setChecked(true);
+            } else {
+                holder.aSwitch.setChecked(false);
+            }
         }
 
         holder.aSwitch.setOnClickListener(new View.OnClickListener() {
@@ -54,10 +67,12 @@ public class PickupAdapter extends RecyclerView.Adapter<PickupAdapter.MyViewHold
                 if (holder.aSwitch.isChecked()){
                     // checked
                     checked = "true";
+//                    Toast.makeText(mContext, pa.pickupMap.get(pickupArrayList.get(position).getPlaceName()), Toast.LENGTH_SHORT).show();
                 } else {
                     // not checked
                     checked = "false";
                 }
+                pa.pickupMap.put(pickupArrayList.get(position).getPlaceName(), checked);
             }
         });
     }
